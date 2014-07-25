3982775b23ba71fcb12c8a09bcc88f635c6b7f04
dgross881 <dgross881@gmail.com>
2014-07-25 13:36:10 +0800

    Add specs for deleteing to-dos

---
+++ spec/features/todo_lists/destroy_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+describe "Editing todo lists" do
+ let!(:todo_list) {TodoList.create(title: "Groceries", description: "Grocery list i created for this tutorial.")}
+ it "is succesful when clicking destory link" do 
+   visit "/todo_lists"
+
+   within "#todo_list_#{todo_list.id}" do
+    	click_link "Destroy"
+   end 
+   expect(page).to_not have_content(todo_list.title)
+   expect(TodoList.count).to eq(0)
+  end
+end 
