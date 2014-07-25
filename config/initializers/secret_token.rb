969fd12a91044458e6ac9a17aa6f019e6709e922
dgross881 <dgross881@gmail.com>
2014-07-25 10:37:17 +0800

    Refactor rspec todo_lsit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,1 @@
+DailyPlanner::Application.config.secret_key_base = 'd20e74923cf4d98f0919d979091c819cd526efd745aaf906ec553020cda6a3708e1c9e2ddde515f9dd6d17f6e7df5b274d071a6f24a13255f2a479476d59d0b0' # replace this
---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper' 
+
+describe "Creating todo lists" do
+  def create_todo_list(options={})
+    options[:title] ||= "Daves weekly planner"
+    options[:description] ||= "On Monday the workouts begin"
+    
+    visit "/todo_lists"
+    click_link "New Todo list" 
+    fill_in "Title", with: options[:title] 
+    fill_in "Description", with: options[:description]
+    click_button "Create Todo list"  
+  end
+
+  it "redirects to the todo list index page on success" do 
+    visit "/todo_lists" 
+    expect(page).to have_content("New Todo list")
+   
+    create_todo_list
+    expect(page).to have_content("Daves weekly planner")
+  end 
+
+  it "displays an error when todo list has no title"  do 
+    expect(TodoList.count).to eq(0)
+    create_todo_list title: "hi"
+    
+    expect(TodoList.count).to eq(0)
+    expect(page).to have_content(" Title is too short (minimum is 3 characters)
+        ")
+    visit "/todo_lists"
+    expect(page).not_to have_content("On Monday the workouts begin")
+  end 
+  it "displays an error with bad descriptions" do 
+    create_todo_list description: "apple"
+
+    expect(page).to have_content("Description is too short")
+    expect(TodoList.count).to eq(0)
+    visit "/todo_lists"
+    expect(page).not_to have_content("apple")
+  end 
+end
