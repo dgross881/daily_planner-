be46cab4c2a3b9d6780ca9e0782ff1f4b1db3668
dgross881 <dgross881@gmail.com>
2014-07-29 19:27:02 +0800

    Add spec helpers

---
+++ spec/features/todo_items/delete_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "Deleting todo items" do
+  let!(:todo_list) {TodoList.create(title: "Daves Workout Schedule", description: "Starting Friday will begin") }
+  let!(:todo_item) {todo_list.todo_items.create(content: "Push ups") } 
+  
+  it "is succesful" do 
+   visit_todo_list(todo_list)
+     within "#todo_item_#{todo_item.id}" do
+     click_link "Delete"
+   end 
+    expect(page).to have_content("Your todo item has been destroyed") 
+    expect(TodoItem.count).to eq(0)
+  end 
+end
