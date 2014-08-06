cb1cebf1e74305ff01ea53e1d8074fc3fd1b919e
dgross881 <dgross881@gmail.com>
2014-08-06 16:33:11 +0800

    Add styling to flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   add_flash_types :success, :failure
+  rescue_from ActiveSupport::MessageVerifier::InvalidSignature, with: :render_error
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
   private 
 
